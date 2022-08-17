ba5e1c57abab728bcc4f57bf33fabf35f9f78d9b
AntonEsipow <esipow.anton99@gmail.com>
2022-08-17 14:35:40 +0300

    Added headers to paged list.

--- app/src/main/java/com/bigtoapp/simplerick/characters/CharacterListPagingEpoxyController.kt
+++ app/src/main/java/com/bigtoapp/simplerick/characters/CharacterListPagingEpoxyController.kt
@@ -4,9 +4,13 @@ import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging.PagedListEpoxyController
 import com.bigtoapp.simplerick.R
 import com.bigtoapp.simplerick.databinding.ModelCharacterListItemBinding
+import com.bigtoapp.simplerick.databinding.ModelCharacterListTitleBinding
+import com.bigtoapp.simplerick.epoxy.LoadingEpoxyModel
 import com.bigtoapp.simplerick.epoxy.ViewBindingKotlinModel
 import com.bigtoapp.simplerick.network.response.GetCharacterByIdResponse
 import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities
 
 class CharacterListPagingEpoxyController: PagedListEpoxyController<GetCharacterByIdResponse>() {
 
@@ -15,6 +19,31 @@ class CharacterListPagingEpoxyController: PagedListEpoxyController<GetCharacterB
         item: GetCharacterByIdResponse?
     ): EpoxyModel<*> {
         return CharacterGridItemEpoxyModel(item!!.image, item.name).id(item.id)
+    }
+
+    override fun addModels(models: List<EpoxyModel<*>>) {
+
+        if(models.isEmpty()) {
+            LoadingEpoxyModel().id("loading").addTo(this)
+            return
+        }
+
+        CharacterGridTitleEpoxyModel("Main Family")
+            .id("main_family_header")
+            .addTo(this)
+
+        super.addModels(models.subList(0, 5))
+
+        (models.subList(5, models.size) as List<CharacterGridItemEpoxyModel>).groupBy {
+            it.name[0].uppercaseChar()
+        }.forEach { mapEntry ->
+            val character = mapEntry.key.toString().uppercase(Locale.US)
+            CharacterGridTitleEpoxyModel(character)
+                .id(character)
+                .addTo(this)
+
+            super.addModels(mapEntry.value)
+        }
     }
 
     data class CharacterGridItemEpoxyModel(
@@ -27,5 +56,18 @@ class CharacterListPagingEpoxyController: PagedListEpoxyController<GetCharacterB
             characterNameTextView.text = name
         }
 
+    }
+
+    data class CharacterGridTitleEpoxyModel(
+        val title: String
+    ): ViewBindingKotlinModel<ModelCharacterListTitleBinding>(R.layout.model_character_list_title) {
+
+        override fun ModelCharacterListTitleBinding.bind() {
+            textView.text = title
+        }
+
+        override fun getSpanSize(totalSpanCount: Int, position: Int, itemCount: Int): Int {
+            return  totalSpanCount
+        }
     }
 }
